ac264a13c054aed2f0fd2e740d3b2227a7b48805
BryanWieschenberg <bryan.wieschenberg@gmail.com>
2025-07-22 15:40:31 -0400

    Added Spring Boot

---
+++ server/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!\n";
+    }
+}
